77706c855b8c6199847cc7762e59d83c297d7db3
booldook <booldook@gmail.com>
2021-11-18 11:57:23 +0900

    App.js axios 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return (
-    
-  );
+  const photoURL = 'https://jsonplaceholder.typicode.com/photos';
+  const [allPhoto, setAllPhoto] = useState([]);
+  const [searchPhoto, setSearchPhoto] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(photoURL);
+        setAllPhoto(data);
+        setSearchPhoto(data);
+        return () => {};
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>Hello world</div>;
 };
 
 export default App;
